48976d0385afd97d6a6a7b44a3b7f1cea6a93ca2
Nahshon <nahshonrm@gmail.com>
2020-02-10 00:00:40 +0200

    10.2/1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Person, PersonsService} from './persons.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'artBank';
+
+export class AppComponent implements OnInit {
+
+  personsRows = [];
+  personsMetaData = [];
+  personsRes = [];
+  displayedColumns: string[] = ['N'];
+  formColumns = [];
+  isAddData: boolean = true;
+
+
+  constructor(private personsService: PersonsService){};
+
+  ngOnInit() {
+    this.personsService.getPersons().subscribe(res => {
+      this.personsRows = res['rows'];
+      this.personsMetaData = res['metaData'];
+      for(let j=0; j< this.personsRows.length; j++){
+        let rows = {'N':j+1};
+        for(let i = 0; i < this.personsMetaData.length; i ++) {
+          rows[this.personsMetaData[i].name] = this.personsRows[j][i];
+        }
+        this.personsRes.push(rows)
+      }
+      console.log(this.personsRes);
+      for (let i=0; i< this.personsMetaData.length; i ++) {
+        this.displayedColumns.push(this.personsMetaData[i].name);
+        this.formColumns.push(this.personsMetaData[i].name);
+      }
+      this.displayedColumns.push('DELETE');
+    }, error => alert(error)
+     );
+  }
+
+  remove(id) {
+    this.personsRes = this.personsRes
+      .filter(res => res['IDCARD'] !== id);
+    // this.personsService.deletePerson(id)
+    //   .subscribe(() => this.ngOnInit(),
+    //     error => alert(error))
+  }
+
+  setIsAddData() {
+    this.isAddData = false;
+  }
+
+  onSubmit(addForm: NgForm) {
+    console.log(addForm.form.value);
+    const data = addForm.form.value;
+    this.personsService.addPerson(data).subscribe(res => {
+      console.log(res)
+    }, error => alert(error) )
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule, MatTableModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
